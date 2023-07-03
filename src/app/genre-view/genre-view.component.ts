9810c3b393bb537f59ea6e8a37a842d758e1fead
Nat-crit20 <greuelnathan74@gmail.com>
2023-07-03 08:20:19 -0500

    Update genre-view.component.ts

--- src/app/genre-view/genre-view.component.ts
+++ src/app/genre-view/genre-view.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-genre-view',
   templateUrl: './genre-view.component.html',
-  styleUrls: ['./genre-view.component.css']
+  styleUrls: ['./genre-view.component.css'],
 })
-export class GenreViewComponent {
+export class GenreViewComponent implements OnInit {
+  constructor(
+    public dialogReg: MatDialogRef<GenreViewComponent>,
+    public data: {
+      genre: any;
+    }
+  ) {}
 
+  ngOnInit(): void {}
 }
